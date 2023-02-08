207592aa0778211bf9a6267eeb5aad9f516e394a
Zu83 <zu83@outlook.fr>
2023-02-08 20:28:55 +0100

    Override account_move line name

--- hiboutik_connector/models/pos.py
+++ hiboutik_connector/models/pos.py
@@ -3,8 +3,8 @@
 
 import logging
 
-from odoo import fields, models, _
-from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -52,53 +52,52 @@ class PosPayment(models.Model):
     hiboutik_payment_detail_id = fields.Integer(
         string='Hiboutik payment detail Id')
 
-    def _create_payment_moves(self):
-        result = self.env['account.move']
-        for payment in self:
-            order = payment.pos_order_id
-            payment_method = payment.payment_method_id
-            if payment_method.type == 'pay_later' or float_is_zero(payment.amount, precision_rounding=order.currency_id.rounding):
-                continue
-            accounting_partner = self.env["res.partner"]._find_accounting_partner(
-                payment.partner_id)
-            pos_session = order.session_id
-            journal = pos_session.config_id.journal_id
-            payment_move = self.env['account.move'].with_context(default_journal_id=journal.id).create({
-                'journal_id': journal.id,
-                'date': payment.pos_order_id.date_order.date(),
-                'ref': _('Invoice payment for %s (%s) using %s - %s') % (order.name, order.account_move.name, payment_method.name, payment.payment_date.date()),
-                'pos_payment_ids': payment.ids,
-            })
-            result |= payment_move
-            payment.write({'account_move_id': payment_move.id})
-            amounts = pos_session._update_amounts({'amount': 0, 'amount_converted': 0}, {
-                                                  'amount': payment.amount}, payment.payment_date)
-            credit_line_vals = pos_session._credit_amounts({
-                # The field being company dependant, we need to make sure the right value is received.
-                'account_id': accounting_partner.with_company(order.company_id).property_account_receivable_id.id,
-                'partner_id': accounting_partner.id,
-                'move_id': payment_move.id,
-                'name': ('%s - test') % payment.payment_date.date()
-            }, amounts['amount'], amounts['amount_converted'])
-            debit_line_vals = pos_session._debit_amounts({
-                'account_id': pos_session.company_id.account_default_pos_receivable_account_id.id,
-                'move_id': payment_move.id,
-                'name': ('%s - test') % payment.payment_date.date()
-            }, amounts['amount'], amounts['amount_converted'])
-            self.env['account.move.line'].with_context(check_move_validity=False).create([
-                credit_line_vals, debit_line_vals])
-            payment_move._post()
-        return result
-
 
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
     # def _post_statement_difference(self, amount):
     #     return {}
+    # @api.depends('payment_method_ids', 'order_ids', 'cash_register_balance_start')
+    # def _compute_cash_balance(self):
+    #     result = super(PosSession, self)._compute_cash_balance()
+    #     for session in self:
+    #         # session.cash_register_balance_end = session.cash_register_total_entry_encoding
+    #         session.cash_register_balance_end = 0.0
+    #         cash_payment_method = session.payment_method_ids.filtered('is_cash_count')[
+    #             :1]
+    #         # session.cash_register_total_entry_encoding = sum(session.mapped('order_ids.payment_ids').filtered(
+    #             # lambda payment: payment.payment_method_id.id == cash_payment_method.id).mapped('amount'))
+
+    #     return result
+
+    @api.depends('payment_method_ids', 'order_ids', 'cash_register_balance_start')
+    def _compute_cash_balance(self):
+        for session in self:
+            cash_payment_method = session.payment_method_ids.filtered('is_cash_count')[
+                :1]
+            if cash_payment_method:
+                total_cash_payment = 0.0
+                last_session = session.search(
+                    [('config_id', '=', session.config_id.id), ('id', '!=', session.id)], limit=1)
+                result = self.env['pos.payment']._read_group([('session_id', '=', session.id), (
+                    'payment_method_id', '=', cash_payment_method.id)], ['amount'], ['session_id'])
+                if result:
+                    total_cash_payment = result[0]['amount']
+                session.cash_register_total_entry_encoding = sum(session.statement_line_ids.mapped('amount')) + (
+                    0.0 if session.state == 'closed' else total_cash_payment
+                )
+                session.cash_register_balance_end = session.cash_register_total_entry_encoding
+                session.cash_register_difference = session.cash_register_balance_end_real - \
+                    session.cash_register_balance_end
+            else:
+                session.cash_register_total_entry_encoding = 0.0
+                session.cash_register_balance_end = 0.0
+                session.cash_register_difference = 0.0
 
     def action_pos_session_closing_control(self, balancing_account=False, amount_to_balance=0, bank_payment_method_diffs=None):
-        result = super(PosSession, self).action_pos_session_closing_control(balancing_account=balancing_account, amount_to_balance=amount_to_balance, bank_payment_method_diffs=bank_payment_method_diffs)
+        result = super(PosSession, self).action_pos_session_closing_control(balancing_account=balancing_account,
+                                                                            amount_to_balance=amount_to_balance, bank_payment_method_diffs=bank_payment_method_diffs)
         # for session in self:
         self.write({'stop_at': self._context.get('stop_at')})
         return result
@@ -110,11 +109,10 @@ class PosSession(models.Model):
             - setting self.move_id to the created account.move record
             - reconciling cash receivable lines, invoice receivable lines and stock output lines
         """
-        _logger.warning('Move %s' % self._context)
         account_move = self.env['account.move'].create({
             'journal_id': self.config_id.journal_id.id,
             'date': self._context.get('stop_at'),
-            'ref': _(('%s - %s') % (self.name, self._context.get('stop_at').date())),
+            'ref': '%s - %s' % (self.name, self._context.get('stop_at').date()),
         })
         self.write({'move_id': account_move.id})
 
@@ -132,25 +130,99 @@ class PosSession(models.Model):
 
         return data
 
+    # def _get_split_receivable_vals(self, payment, amount, amount_converted):
+    #     accounting_partner = self.env["res.partner"]._find_accounting_partner(
+    #         payment.partner_id)
+    #     if not accounting_partner:
+    #         raise UserError(_("You have enabled the \"Identify Customer\" option for %s payment method,"
+    #                           "but the order %s does not contain a customer.") % (payment.payment_method_id.name,
+    #                                                                               payment.pos_order_id.name))
+    #     partial_vals = {
+    #         'account_id': accounting_partner.property_account_receivable_id.id,
+    #         'move_id': self.move_id.id,
+    #         'partner_id': accounting_partner.id,
+    #         'name': '%s - %s %s' % (self.name, self._context.get('stop_at').strftime("%d/%m/%Y"), payment.payment_method_id.name),
+    #     }
+    #     return self._debit_amounts(partial_vals, amount, amount_converted)
+
     def _get_split_receivable_vals(self, payment, amount, amount_converted):
-        accounting_partner = self.env["res.partner"]._find_accounting_partner(
-            payment.partner_id)
-        if not accounting_partner:
-            raise UserError(_("You have enabled the \"Identify Customer\" option for %s payment method,"
-                              "but the order %s does not contain a customer.") % (payment.payment_method_id.name,
-                                                                                  payment.pos_order_id.name))
-        partial_vals = {
-            'account_id': accounting_partner.property_account_receivable_id.id,
-            'move_id': self.move_id.id,
-            'partner_id': accounting_partner.id,
-            'name': '%s - %s %s' % (self.name, self._context.get('stop_at').date(), payment.payment_method_id.name),
-        }
-        return self._debit_amounts(partial_vals, amount, amount_converted)
+        partial_vals = super()._get_split_receivable_vals(
+            payment, amount, amount_converted)
+        partial_vals['name'] = '%s - %s %s' % (self.name, self._context.get(
+            'stop_at').strftime("%d/%m/%Y"), payment.payment_method_id.name)
+        return partial_vals
+
+    # def _get_combine_receivable_vals(self, payment_method, amount, amount_converted):
+    #     partial_vals = {
+    #         'account_id': self._get_receivable_account(payment_method).id,
+    #         'move_id': self.move_id.id,
+    #         'name': '%s - %s %s' % (self.name, self._context.get('stop_at').strftime("%d/%m/%Y"), payment_method.name)
+    #     }
+    #     return self._debit_amounts(partial_vals, amount, amount_converted)
 
     def _get_combine_receivable_vals(self, payment_method, amount, amount_converted):
-        partial_vals = {
-            'account_id': self._get_receivable_account(payment_method).id,
-            'move_id': self.move_id.id,
-            'name': '%s - %s %s' % (self.name, self._context.get('stop_at').date(), payment_method.name)
-        }
-        return self._debit_amounts(partial_vals, amount, amount_converted)
+        partial_vals = super()._get_combine_receivable_vals(
+            payment_method, amount, amount_converted)
+        partial_vals['name'] = '%s - %s %s' % (self.name, self._context.get(
+            'stop_at').strftime("%d/%m/%Y"), payment_method.name)
+        return partial_vals
+
+    # def _get_sale_vals(self, key, amount, amount_converted):
+    #     account_id, sign, tax_keys, base_tag_ids = key
+    #     tax_ids = set(tax[0] for tax in tax_keys)
+    #     applied_taxes = self.env['account.tax'].browse(tax_ids)
+    #     title = 'Sales' if sign == 1 else 'Refund'
+    #     name = '%s untaxed' % title
+    #     if applied_taxes:
+    #         name = _('%s - %s %s with %s' % (self.name, self._context.get('stop_at').strftime(
+    #             "%d/%m/%Y"), title, ', '.join([tax.name for tax in applied_taxes])))
+    #     partial_vals = {
+    #         'name': name,
+    #         'account_id': account_id,
+    #         'move_id': self.move_id.id,
+    #         'tax_ids': [(6, 0, tax_ids)],
+    #         'tax_tag_ids': [(6, 0, base_tag_ids)],
+    #     }
+    #     return self._credit_amounts(partial_vals, amount, amount_converted)
+
+    def _get_sale_vals(self, key, amount, amount_converted):
+        partial_vals = super()._get_sale_vals(key, amount, amount_converted)
+        account_id, sign, tax_keys, base_tag_ids = key
+        tax_ids = set(tax[0] for tax in tax_keys)
+        applied_taxes = self.env['account.tax'].browse(tax_ids)
+        title = _('Sales') if sign == 1 else _('Refund')
+        untaxed = _('untaxed')
+        name = '%s %s' % (title, untaxed)
+        if applied_taxes:
+            name = '%s - %s %s %s' % (self.name, self._context.get('stop_at').strftime(
+                "%d/%m/%Y"), title, ', '.join([tax.name for tax in applied_taxes]))
+
+        partial_vals['name'] = name
+
+        return partial_vals
+
+    # def _get_tax_vals(self, key, amount, amount_converted, base_amount_converted):
+    #     account_id, repartition_line_id, tax_id, tag_ids = key
+    #     tax = self.env['account.tax'].browse(tax_id)
+    #     name = _('%s - %s %s' % (self.name,
+    #              self._context.get('stop_at').strftime("%d/%m/%Y"), tax.name))
+    #     partial_args = {
+    #         'name': name,
+    #         'account_id': account_id,
+    #         'move_id': self.move_id.id,
+    #         'tax_base_amount': abs(base_amount_converted),
+    #         'tax_repartition_line_id': repartition_line_id,
+    #         'tax_tag_ids': [(6, 0, tag_ids)],
+    #     }
+    #     return self._debit_amounts(partial_args, amount, amount_converted)
+
+    def _get_tax_vals(self, key, amount, amount_converted, base_amount_converted):
+        partial_args = super()._get_tax_vals(
+            key, amount, amount_converted, base_amount_converted)
+        account_id, repartition_line_id, tax_id, tag_ids = key
+        tax = self.env['account.tax'].browse(tax_id)
+        name = '%s - %s %s' % (self.name,
+                               self._context.get('stop_at').strftime("%d/%m/%Y"), tax.name)
+        partial_args['name'] = name
+
+        return partial_args
